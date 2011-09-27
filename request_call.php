921669fa0feed2f3a3e5ff71ac2d3fa2ba17fcfb
Steven Lu <tacticalazn@gmail.com>
2011-09-26 22:26:34 -0400

    Added in jQuery forms and emailer.

---
+++ request_call.php
@@ -0,0 +1,45 @@
+<?php
+include 'include/class.phpmailer.php';
+
+$name = $_POST['name'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
+
+// Sending an email to the client.
+$mail = new PHPMailer();
+
+$mail->From = "webresponseteam@ns-email.com";
+$mail->FromName = "NetServices Web-Response Team";
+
+$mail->Subject = "Your Call Request Was Submitted";
+$body = "Dear Valued Client,\n\n";
+$body .= "Your call request has been sent. Someone should be contacting you soon.\n";
+$body .= "If this is urgent, you can always call us at 1-724-NET-SERV (1-724-638-7378\n\n";
+$body .= "Regards,\nThe NetServices Web-Response Team";
+
+$mail->Body = $body;
+$mail->AddAddress($email, $name);
+$mail->Send();
+
+// Sending an email to the response team.
+$mail = new PHPMailer();
+
+$mail->From = $email;
+$mail->FromName = $name;
+
+$mail->Subject = "TheNetServices.com Form - Request a Call (From Header)";
+$body = "Dear Representative,\n\n";
+$body .= "The following visitor has requested a call from the Web site header.\n\n";
+$body .= "Please call:\n\n";
+$body .= "Name:                  " . $name . "\n";
+$body .= "Telephone Number:      " . $phone . "\n\n";
+$body .= "-\n\n";
+$body .= "Request Date & Time:   " . date('F jS Y h:i:s A') . "\n";
+$body .= "IP Address of Visitor: " . $_SERVER['REMOTE_ADDR'];
+
+$mail->Body = $body;
+$mail->AddAddress("webresponseteam@ns-email.com", "NetServices Web-Response Team");
+$mail->Send();
+
+echo 'true';
+?>
---
+++ resources/js/forms.js
@@ -0,0 +1,82 @@
+$(document).ready(function() {
+   $('#request_call').ajaxForm({
+      dataType: 'json',
+      success: processCallRequest,
+      beforeSubmit: validateCallRequest
+   });
+
+});
+
+function showError(message)
+{
+   $('#request_message').html("ERROR: "+message);
+   $('#request_table').fadeTo('normal', 0, function()
+   {
+      $('#request_message').fadeTo('normal', 1, function()
+      {
+         $('#request_message').delay(1500).slideUp('fast');
+         $('#request_message').delay(1500).fadeTo('normal', 0);
+         $('#request_table').delay(1500).fadeTo('normal', 1);
+      });
+   });
+}
+
+function validateCallRequest(formData, jqForm, options)
+{
+   var form = jqForm[0];
+
+   var name = form.name.value;
+   var phone_number = form.phone.value;
+   var email = form.email.value;
+
+	if (name == "")
+	{
+		showError("You did not provide us a name.");
+		return false;
+	}
+	
+	if (phone_number == "")
+	{
+		showError("You need to provide us a phone number to call.");
+		return false;
+	}
+	
+	if (email == "")
+	{
+		showError("You need to give us an email.");
+		return false;
+	}
+	
+	var email_syntax = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+	
+	if (email_syntax.test(email) == false)
+	{
+		showError("The email address you provided does not seem to be valid");
+		return false;
+	}
+	
+	var phone_stripped = phone_number.replace(/[\(\)\.\-\ ]/g, '');
+	if (isNaN(parseInt(phone_stripped)))
+	{
+		showError("The phone number you provided doesn't seem to be valid.");
+		return false;
+	}
+	
+	if (!(phone_stripped.length == 10))
+	{
+		showError("You need to provide is a 10 digit phone number.");
+		return false;
+	}
+	
+	return true;
+}
+
+function processCallRequest(response)
+{
+   if (response == true)
+   {
+      $('#request_message').html("Your call request has been submitted.");
+      $('#request_message').fadeTo('normal', 1);
+      $('#request_table').fadeTo('normal', 0);
+   }   
+}
