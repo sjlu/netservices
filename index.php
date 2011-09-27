d5cef56f7719633c1c1281a6cb44400277a2f37c
Steven Lu <tacticalazn@gmail.com>
2011-09-26 22:54:31 -0400

    added in index photo

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 	<?php include_once 'include/top.html'; ?>
    <?php include_once 'include/boxes.html'; ?>
 	<div id="content">
+      <img src="resources/img/index-people.jpg" style="margin-bottom:-30px;" alt="people" />
    </div>
 	<?php include_once 'include/footer.html'; ?>
 </body>
