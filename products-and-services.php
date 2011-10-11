005e872fc61326fbba6e3adc036b32876fef312c
Steven Lu <tacticalazn@gmail.com>
2011-10-11 01:11:45 -0400

    layout changes, jquery bug fixes

--- products-and-services.php
+++ products-and-services.php
@@ -4,26 +4,56 @@
 	<div id="content">
       <table border="0">
       <tr>
-         <td width="50%"><img src="resources/img/index-people.jpg" width="90%" /></td>
-         <td width="50%">
-      <h1 id="services-for-existing-web-sites">Services for Existing Web Sites</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor pharetra tortor, at rutrum magna facilisis ut. Praesent laoreet leo non metus viverra convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi tellus neque, gravida sed fringilla sit amet, ultrices sed nunc. Morbi quis ligula sit amet risus porttitor dignissim. Proin pulvinar hendrerit tincidunt. Maecenas mattis nulla elementum magna dignissim ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam pretium sodales urna eget porttitor. Sed risus purus, pretium eget volutpat nec, pretium vel diam. Sed sit amet sem dui.</p>
-      <h1 id="online-repouation-management">Online Reputation Management</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor pharetra tortor, at rutrum magna facilisis ut. Praesent laoreet leo non metus viverra convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi tellus neque, gravida sed fringilla sit amet, ultrices sed nunc. Morbi quis ligula sit amet risus porttitor dignissim. Proin pulvinar hendrerit tincidunt. Maecenas mattis nulla elementum magna dignissim ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam pretium sodales urna eget porttitor. Sed risus purus, pretium eget volutpat nec, pretium vel diam. Sed sit amet sem dui.</p>
+         <td width="40%" style="text-align: center;"><img src="http://placehold.it/350x200"/></td>
+         <td width="60%">
+		      <h1 id="services-for-existing-web-sites">Services for Existing Web Sites</h1>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor pharetra tortor, at rutrum magna facilisis ut. Praesent laoreet leo non metus viverra convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi tellus neque, gravida sed fringilla sit amet, ultrices sed nunc. Morbi quis ligula sit amet risus porttitor dignissim. Proin pulvinar hendrerit tincidunt. Maecenas mattis nulla elementum magna dignissim ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam pretium sodales urna eget porttitor. Sed risus purus, pretium eget volutpat nec, pretium vel diam. Sed sit amet sem dui.</p>
+
+			</td>
+		</tr>
+		</table>
+		
+		<table border="0">
+		<tr>
+			<td width="60%">
+				<h1 id="online-repouation-management">Online Reputation Management</h1>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor pharetra tortor, at rutrum magna facilisis ut. Praesent laoreet leo non metus viverra convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi tellus neque, gravida sed fringilla sit amet, ultrices sed nunc. Morbi quis ligula sit amet risus porttitor dignissim. Proin pulvinar hendrerit tincidunt. Maecenas mattis nulla elementum magna dignissim ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam pretium sodales urna eget porttitor. Sed risus purus, pretium eget volutpat nec, pretium vel diam. Sed sit amet sem dui.</p>
          </td>
+			<td width="40%" style="text-align: center;"><img src="http://placehold.it/350x200"/></td>
       </tr>
+		</table>
+		
+      <table border="0">
       <tr>
-         <td>
-      <h1 id="social-media-management">Social Media Management</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor pharetra tortor, at rutrum magna facilisis ut. Praesent laoreet leo non metus viverra convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi tellus neque, gravida sed fringilla sit amet, ultrices sed nunc. Morbi quis ligula sit amet risus porttitor dignissim. Proin pulvinar hendrerit tincidunt. Maecenas mattis nulla elementum magna dignissim ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam pretium sodales urna eget porttitor. Sed risus purus, pretium eget volutpat nec, pretium vel diam. Sed sit amet sem dui.</p>
-      <h1 id="seo">SEO - Search Engine Optimization</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor pharetra tortor, at rutrum magna facilisis ut. Praesent laoreet leo non metus viverra convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi tellus neque, gravida sed fringilla sit amet, ultrices sed nunc. Morbi quis ligula sit amet risus porttitor dignissim. Proin pulvinar hendrerit tincidunt. Maecenas mattis nulla elementum magna dignissim ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam pretium sodales urna eget porttitor. Sed risus purus, pretium eget volutpat nec, pretium vel diam. Sed sit amet sem dui.</p>
-      <h1 id="web-design-and-web-development">Web Design and Web Development</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor pharetra tortor, at rutrum magna facilisis ut. Praesent laoreet leo non metus viverra convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi tellus neque, gravida sed fringilla sit amet, ultrices sed nunc. Morbi quis ligula sit amet risus porttitor dignissim. Proin pulvinar hendrerit tincidunt. Maecenas mattis nulla elementum magna dignissim ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam pretium sodales urna eget porttitor. Sed risus purus, pretium eget volutpat nec, pretium vel diam. Sed sit amet sem dui.</p>
+         <td width="40%" style="text-align: center;"><img src="http://placehold.it/350x200"/></td>
+         <td width="60%">
+		      <h1 id="social-media-management">Social Media Management</h1>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor pharetra tortor, at rutrum magna facilisis ut. Praesent laoreet leo non metus viverra convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi tellus neque, gravida sed fringilla sit amet, ultrices sed nunc. Morbi quis ligula sit amet risus porttitor dignissim. Proin pulvinar hendrerit tincidunt. Maecenas mattis nulla elementum magna dignissim ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam pretium sodales urna eget porttitor. Sed risus purus, pretium eget volutpat nec, pretium vel diam. Sed sit amet sem dui.</p>
+
+			</td>
+		</tr>
+		</table>
+		
+		<table border="0">
+		<tr>
+			<td width="60%">
+				<h1 id="seo">SEO - Search Engine Optimization</h1>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor pharetra tortor, at rutrum magna facilisis ut. Praesent laoreet leo non metus viverra convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi tellus neque, gravida sed fringilla sit amet, ultrices sed nunc. Morbi quis ligula sit amet risus porttitor dignissim. Proin pulvinar hendrerit tincidunt. Maecenas mattis nulla elementum magna dignissim ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam pretium sodales urna eget porttitor. Sed risus purus, pretium eget volutpat nec, pretium vel diam. Sed sit amet sem dui.</p>
          </td>
-         <td width="50%"><img src="resources/img/index-people.jpg" width="90%" /></td>
+			<td width="40%" style="text-align: center;"><img src="http://placehold.it/350x200"/></td>
       </tr>
-      </table>
+		</table>
+
+      <table border="0">
+      <tr>
+         <td width="40%" style="text-align: center;"><img src="http://placehold.it/350x200"/></td>
+         <td width="60%">
+
+		      <h1 id="web-design-and-web-development">Web Design and Web Development</h1>
+		      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor pharetra tortor, at rutrum magna facilisis ut. Praesent laoreet leo non metus viverra convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi tellus neque, gravida sed fringilla sit amet, ultrices sed nunc. Morbi quis ligula sit amet risus porttitor dignissim. Proin pulvinar hendrerit tincidunt. Maecenas mattis nulla elementum magna dignissim ultrices. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aliquam pretium sodales urna eget porttitor. Sed risus purus, pretium eget volutpat nec, pretium vel diam. Sed sit amet sem dui.</p>
+			</td>
+		</tr>
+		</table>
    </div>
 	<?php include_once 'include/footer.html'; ?>
 </body>	
