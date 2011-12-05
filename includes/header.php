3b3cf254bd3ac96dd545b6e7efa0c0e9bb1938da
Steven Lu <tacticalazn@gmail.com>
2011-12-05 11:46:26 -0500

    Added landing page.

--- includes/header.php
+++ includes/header.php
@@ -5,7 +5,11 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
         "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <head>
+   <?php if (file_exists('resources/css/' . $filename . '.css')): ?>
+   <link rel="stylesheet" type="text/css" media="screen" href="resources/css/<?= $filename ?>.css" />
+   <?php else: ?>
    <link rel="stylesheet" type="text/css" media="screen" href="resources/css/style.css" />
+   <?php endif ?>
 
    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
    <script type="text/javascript" src="resources/js/jquery.form.js"></script>
@@ -17,12 +21,10 @@
       DD_belatedPNG.fix('.png-fix');
    </script>
 
-   <?php if (file_exists('' . $filename . '.meta')) include '' . $filename . '.meta';?>
-
-   <?php if (!file_exists('' . $filename . '.meta')): ?>
+   <?php if (file_exists('' . $filename . '.meta')) include '' . $filename . '.meta'; else { ?>
    <title>TheNetServices.com</title>
    <meta name="author" content="NetServices, LLC" />
    <meta name="description" content="" />
    <meta name="keywords" content="" />
-   <?php endif ?>
+   <?php } ?>
 </head>
---
+++ landing.php
@@ -0,0 +1,25 @@
+<?php include_once 'includes/header.php'; ?>
+
+<div id="header">
+   <div style="width: 1000px; margin: 0 auto;">
+      <img src="resources/img/logo.png" style="margin: 85px 0 0 70px;" valign="top" />
+      <img src="resources/img/landing_page-consultant.png" style="margin: 20px 0 0 260px;" />
+   </div>
+</div>
+
+<div id="container">
+   <div id="content">
+      <div id="form">
+
+
+      </div>
+      <div id="box">
+
+
+      </div>
+      <div id="text">
+
+
+      </div>
+   </div>
+</div>
