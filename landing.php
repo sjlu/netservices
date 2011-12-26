85f5b61b714d13cc153a2d2fbf5fd37635b1b8dd
Steven Lu <tacticalazn@gmail.com>
2011-12-26 13:48:31 -0500

    Landing fixes? Updated new images

--- landing.php
+++ landing.php
@@ -2,7 +2,7 @@
 
 <div id="header">
    <div style="width: 1000px; margin: 0 auto;">
-      <img src="resources/img/logo.png" style="margin: 85px 0 0 70px;" valign="top" />
+      <img src="resources/img/logo.png" style="margin: 85px 0 0 40px;" valign="top" />
       <img src="resources/img/landing_page-consultant.png" style="margin: 20px 0 0 260px;" />
    </div>
 </div>
@@ -14,12 +14,18 @@
 
       </div>
       <div id="box">
-
-
+         <div id="buttons" style="margin: 40px 0 0 110px;">
+            <button type="submit" class="button_submit"></button>
+            <button type="submit" class="button_continue"></button>
+         </div>
       </div>
       <div id="text">
-
-
+         <h1>Sample Title</h1>
+         <p>Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text</p>
+         <h1>Sample Title</h1>
+         <p>Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text</p>
+         <h1>Sample Title</h1>
+         <p>Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text Sample Text</p>
       </div>
    </div>
 </div>
